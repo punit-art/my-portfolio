dd0e403c9fe10e8b8aa449bb1776fa0c3cc7071d
punit-art <punitmundotiya24@gmail.com>
2025-09-10 17:47:02 +0530

    Describe your changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+import { BrowserRouter } from "react-router-dom";
+
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+  <App />
+</BrowserRouter>
+
